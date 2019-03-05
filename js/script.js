f0f06b6d7e065be6fd3d906f32787c0f4fcc5ae3
Ian Jeff <iankajiji@gmail.com>
2019-03-05 12:20:01 +0300

    add more js functions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-
 $(document).ready(function() {
-    $("#info form").submit(function(event) {
-        var player1input = $("input#player1").val();
-        $(".player1").append(player1Input);
-        $("#story").show();
-});
+  $("#info form").submit(function(event) {
+    var player1Input1 = ("input#player1").val();
+    $(".player1").append(player1Input);
+    $("#story").show();
 
-        $("#info form").submit(function(event) {
-            var player2input = $("input#player2").val();
-            $(".player2").append(player2Input);
-            $("#story").show();
-
-  event.preventDefault();
+    event.preventDefault();
+  });
 });
-}
